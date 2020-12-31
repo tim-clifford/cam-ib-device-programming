09e44b8a2e5784af28f45312b66732476b81b43d
Tim Clifford <tclifford@protonmail.com>
2020-12-31 01:00:43 +0000

    Debug compile-time

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include "mbed.h"
 
-DigitalIn button(USER_BUTTON);
-DigitalOut led2(LED2);
-DigitalOut led3(LED3);
+DigitalOut myled(LED1);
 
-int main()
-{
-  led2=0;
-  while(true)
-  {
-	led3 = button == 0;
-	led2 = !led3;
-	ThisThread::sleep_for(20); // 20 ms
-  }
+int main() {
+        while(true) {
+                myled = 1; // LED is ON
+                ThisThread::sleep_for(1000*0.2); // 200 ms
+                myled = 0; // LED is OFF
+                ThisThread::sleep_for(1000*1.0); // 1 sec
+        }
 }
